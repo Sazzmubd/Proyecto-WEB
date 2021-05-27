bbcf6709f3437a55674de8316453ba78e5638b6e
Marc <dlcmarc32@gmail.com>
2021-05-27 21:02:33 +0200

    admin y solicitudes conectados a bbdd

--- src/app/admin.php
+++ src/app/admin.php
@@ -7,7 +7,7 @@ $conexion = mysqli_connect('localhost', 'root', '', 'proyecto');
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Lista de Solicitudes</title>
+    <title>Lista de clientes</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -19,62 +19,57 @@ $conexion = mysqli_connect('localhost', 'root', '', 'proyecto');
 
 <body>
 
-<!-- Menú -->
-<nav id="mySidenav" class="sidenav">
+<nav id="mySidenav" class="sidenav"><!-- LO DE LOS UL -->
     <ul>
         <li><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&#215;</a></li>
-        <li><a class="sidenav-nav" href="../preguntasFrecuentes.html">Preguntas Frecuentes</a></li>
-        <li><a class="sidenav-nav" href="monitorSensores2.html">Sensores</a></li>
+        <li><a class="sidenav-nav" href="tablaSolicitudes.html">Tabla Solicitudes</a></li>
     </ul>
 </nav>
 
-<!-- Cabezera -->
+
 <header class="header">
     <div class="container logo-menu-container">
         <a href="../index.html" class="logo"><img src="ImagenesProyecto/logoGTI.svg" height="" width="120"/></a>
         <!-- Use any element to open the sidenav -->
-        <span><a class="menu-icon" onClick="openNav()"><img src="ImagenesProyecto/LandingPage/menu.svg" height="32" width="46" alt="menu-icon"/></a></span>
+        <span><a class="menu-icon" onClick="openNav()"><img src="ImagenesProyecto/LandingPage/menu.svg"
+                                                            height="32" width="46" alt="menu-icon"/></a></span>
     </div>
 </header>
 
 <main>
 
-    <div class="titulo-usuario-box"><h2 class="titulo">Lista de solicitudes</h2></div>
-
+    <div class="titulo-usuario-box"><h2 class="titulo">Lista de clientes</h2></div>
 
     <br>
 
     <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Filtrar por...">
 
-    <!-- Tabla de solicitudes1 -->
     <table class="sortable" id="myTable">
         <tr>
-            <th>Fecha</th>
-            <th>Nombre</th>
+            <th>Nombre y Apellidos</th>
             <th>Tipo</th>
-            <th>Email</th>
+            <th>Correo</th>
             <th>Teléfono</th>
-            <th>Motivo</th>
-            <th>Provincia</th>
-            <th>Añadir/Eliminar</th>
+            <th>Visualizar Sensores</th>
+            <th>Editar</th>
+
         </tr>
 
         <?php
-        $sql="SELECT * from solicitud";
+        $sql="SELECT * from clientes";
         $result=mysqli_query($conexion,$sql);
 
         while($mostrar=mysqli_fetch_array($result)){
         ?>
 
         <tr>
-            <td data-titulo="Fecha"><div style="width: 6rem"><?php echo $mostrar['fecha']?></div></td>
-            <td data-titulo="Nombre"><?php echo $mostrar['nombreApellidosEmpresa']?></td>
-            <td data-titulo="Apellidos"><?php echo $mostrar['tipo']?></td>
-            <td data-titulo="Email"><?php echo $mostrar['correo']?></td>
+            <td data-titulo="Nombre y Apellidos"><?php echo $mostrar['nombreapellidosempresa']?></td>
+            <td data-titulo="Tipo"><?php echo $mostrar['tipo']?></td>
+            <td data-titulo="Correo"><?php echo $mostrar['correo']?></td>
             <td data-titulo="Teléfono"><?php echo $mostrar['telefono']?></td>
-            <td data-titulo="Motivo"><?php echo $mostrar['motivo']?></td>
-            <td data-titulo="Provincia"><?php echo $mostrar['provincia']?></td>
-            <td><div><img src="ImagenesProyecto/plus.svg" height="" width="30">  <img src="ImagenesProyecto/eliminar.svg" height="" width="30"></div></td>
+            <td><a href="../app/monitorSensores2.html" class="logo"><img src="ImagenesProyecto/atencion.svg" height="" width="70"/></a></td>
+            <td><img src="ImagenesProyecto/editar.svg" height="" width="20"</td>
+
         </tr>
             <?php
         }
@@ -84,16 +79,13 @@ $conexion = mysqli_connect('localhost', 'root', '', 'proyecto');
     <br><br><br>
 </main>
 
-<!-- Footer -->
-<footer id="footer" class="footer">
-    <p><a href="../terminosPrivacidad.html">© 2021 - GTI</a></p>
+<footer class="footer">
+    <p>© 2021 - GTI</p>
     <p>Escuela Politécnica Superior de Gandia <br>
         C/ Paranimf, 1 <br>
         46730 Grao de Gandia, Gandia <br>
-        Tel. (+34) 96 284 93 33
-    </p>
+        Tel. (+34) 96 284 93 33</p>
 </footer>
-
 
 <!-- Scripts -->
 <script src="js/jQuery.js"></script>
