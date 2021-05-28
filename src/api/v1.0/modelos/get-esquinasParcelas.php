5f3dca37d8d12a4ab06d6c27d280bafc484bd0f6
alvaroescr0008 <72156197+alvaroescr0008@users.noreply.github.com>
2021-05-28 08:50:31 +0200

    newMapa

--- src/api/v1.0/modelos/get-esquinasParcelas.php
+++ src/api/v1.0/modelos/get-esquinasParcelas.php
@@ -8,6 +8,6 @@ $res = mysqli_query($conn, $sql);
 while($fila = mysqli_fetch_assoc($res)){//fila es lo importante, q es lo que despue se manda al JSSSSS
     array_push($salida, $fila);
 }
-echo json_encode($salida);//enviar a  JSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+//echo json_encode($salida);//enviar a  JSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
 
 $http_code = 200;
