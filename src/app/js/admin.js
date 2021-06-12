b2299a8d3707f54b721cfdc7a13f1dae6a118735
Marc <dlcmarc32@gmail.com>
2021-06-12 20:24:45 +0200

    php en html arreglado

---
+++ src/app/js/admin.js
@@ -0,0 +1,20 @@
+fetch("../api/v1.0/modelos/admin.php",{
+    method:"GET"
+}).then(function (respuesta){
+    if(respuesta.ok){//si la repuesta devuleve un ok
+        console.log("ok");
+        return respuesta.json();
+    }
+}).then(function (clientes){
+    console.log(clientes);
+
+    for (let i = 0; i < clientes.length; i++) {
+        document.getElementById("myTable").innerHTML=document.getElementById("myTable").innerHTML+
+            '<td id="nombreyapellidos" data-titulo="Nombre y Apellidos">'+clientes[i].nombreapellidosempresa+'</td>'+
+            '<td id="tipo" data-titulo="Tipo">'+clientes[i].tipo+'</td>'+
+            '<td id="email" data-titulo="Email">'+clientes[i].correo+'</td>'+
+            '<td id="telefono" data-titulo="Teléfono">'+clientes[i].telefono+'</td>'+
+            '<td>'+'<a href="../app/monitorSensores2.html" className="logo">'+'<img src="ImagenesProyecto/atencion.svg" height="" width="70"/>'+'</a>'+'</td>'+
+            '<td>'+'<img src="ImagenesProyecto/editar.svg" height="" width="20">'+'</td>'
+    }
+})
--- src/app/js/solicitudes.js
+++ src/app/js/solicitudes.js
@@ -1,4 +1,4 @@
-fetch("../app/tablaSolicitudes.php",{
+fetch("../api/v1.0/modelos/tablaSolicitudes.php",{
     method:"GET"
 }).then(function (respuesta){
     if(respuesta.ok){//si la repuesta devuleve un ok
@@ -6,5 +6,16 @@ fetch("../app/tablaSolicitudes.php",{
         return respuesta.json();
     }
 }).then(function (solicitudes){
-    console.log(solicitudes)
+    console.log(solicitudes);
+
+    for (let i = 0; i < solicitudes.length; i++) {
+        document.getElementById("myTable").innerHTML=document.getElementById("myTable").innerHTML+'<td data-titulo="Fecha">'+'<div style="width: 6rem" id="fecha">'+solicitudes[i].fechasolicitud+'</div>'+'</td>'+
+            '<td id="nombreyapellidos" data-titulo="Nombre y Apellidos">'+solicitudes[i].nombreApellidosEmpresa+'</td>'+
+            '<td id="tipo" data-titulo="Tipo">'+solicitudes[i].tipo+'</td>'+
+            '<td id="email" data-titulo="Email">'+solicitudes[i].correo+'</td>'+
+            '<td id="telefono" data-titulo="Teléfono">'+solicitudes[i].telefono+'</td>'+
+            '<td id="motivo" data-titulo="Motivo">'+solicitudes[i].motivo+'</td>'+
+            '<td id="provincia" data-titulo="Provincia">'+solicitudes[i].provincia+'</td>'+
+            '<td>'+'<div>'+'<img src="ImagenesProyecto/plus.svg" height="" width="30">'+'<img src="ImagenesProyecto/eliminar.svg" height="" width="30">'+'</div>'+'</td>'
+    }
 })
